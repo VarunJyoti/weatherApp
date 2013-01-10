c64005e6d5ae656b0678b69f9cada42ff3e6552f
Varun Kashyap <varunkashyapcse@gmail.com>
2013-01-10 12:28:57 +0530

    Testing github flow

--- app/view/Main.js
+++ app/view/Main.js
@@ -25,7 +25,7 @@ Ext.define('weather.view.Main', {
                 html: [
                     "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
                     "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
+                    "and refresh to change what's rendered here. Changed by VK"
                 ].join("")
             },
             {
