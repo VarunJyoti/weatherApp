09869bfb1c037542d03e039b17a8e709bc0b9421
Varun Kashyap <varunkashyapcse@gmail.com>
2013-01-10 13:16:02 +0530

    Cleaning up views

--- app/view/Main.js
+++ app/view/Main.js
@@ -19,20 +19,16 @@ Ext.define('weather.view.Main', {
                 html: [
                     "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
                     "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here. Changed by VK"
+                    "and refresh to change what's rendered here."
                 ].join("")
             },
             {
                 title: 'Get Started',
                 iconCls: 'action',
 
-                items: [
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
+                html: [
+                    "Another place holder view for the time being"
+                ].join("")
             }
         ]
     }
